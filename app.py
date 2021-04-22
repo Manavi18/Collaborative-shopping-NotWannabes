f6b5f8d86d78eb8ffa8bf4973158061ed7619e78
Manavi <58180058+Manavi18@users.noreply.github.com>
2021-04-22 09:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,145 +1,144 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_socketio import SocketIO, send
-import sqlite3, hashlib
+from flask import Flask, render_template, request,session, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import update
+from models import *
+import hashlib
+from flask_socketio import SocketIO, send, emit, join_room
+
 
 app = Flask("__name__")
-app.config['SECRET_KEY'] = 'mysecret'
 
+app.secret_key = 'secret@123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///C:\\Users\\manavi\\Desktop\\python\\myn\\myn.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 socketio = SocketIO(app)
-ROOMS = set()
 
-section = []
-# listening for a message event
-@socketio.on('message')
-def handle_message(data):
-    # when message is received then broadcast to all clients that are connected
-    send(data, broadcast=True)
-    # send((data.username,data.msg))
-    section.append(data['username'])
-    for i in section:
-        conn = sqlite3.connect("database1.db")
-        c = conn.cursor()
-        c.execute('INSERT INTO kart (name) VALUES (?)', (i,))
-        conn.commit()
-        conn.close()
-
-
-@socketio.on('new_room')
-def new_room(data):
-    ROOMS.add(data['new_room_name'])
-
-
-@app.route('/')
+@app.route("/")
 def welcome():
-    return render_template("welcome.html")
+    return render_template('welcome.html')
 
-
-@app.route('/home1', methods=['GET','POST'])
-def home1():
-    customer_in_session = []
-
+@app.route("/home",methods=['GET','POST'])
+def home():
     if request.method == 'POST':
         # receiving option chosen from client
         section = str(request.form.get('opt1'))
 
         # converting it into a list section user name and product chosen for that user
         prod_name = section.split(',')
+        print(prod_name[1])
+        
+        prod_name.append([Product.query.all()[i].product_url for i in range(len(Product.query.all())) if Product.query.all()[i].product_name==prod_name[1]])
 
-        con = sqlite3.connect('database1.db')
-        cur = con.cursor()
+        
+        prod_name.append([Grpchat.query.all()[i].hostid for i in range(len(Grpchat.query.all()))][0])
+        # print(type(prod_name[0]))
 
-        # getting the corresponding image url fro the products table
-        cur.execute('SELECT img FROM products WHERE name="'+prod_name[1]+'"')
-        image = cur.fetchone()
+        # [grpchat.query.all()[].hostid][0]
+        if len(Grpcart.query.all()) == 3:
+            Grpcart.update(str(prod_name[0]),product_name=str(prod_name[1],),product_img=str(prod_name[2]))
+            # Grpcart.query.filter_by(sectionid=str(prod_name[0])).update(product_name=str(prod_name[1]))
 
-        # updating name corresponding product and corresponding image url into kart..this information will be used while creating sections
-        cur.execute('UPDATE kart SET prod_name="'+prod_name[1]+'",prod_img="'+str(image)[2:-3]+'" WHERE name="'+prod_name[0]+'"')
-
-        con.commit()
-        con.close()
-
-    con = sqlite3.connect('database1.db')
-    cur = con.cursor()
-
-    # sending products to client to display and names for creating options in the dropdown
-    cur.execute('SELECT * FROM products')
-    itemData = cur.fetchall()
-    cur.execute('SELECT name FROM kart')
-    data = cur.fetchall()
-
-    for i in data:
-        customer_in_session.append(str(i)[2:-3])
-    customer_in_session = set(customer_in_session)
-
-    username = request.args.get('username', None)
-    return render_template("home1.html", username=username, rooms=list(ROOMS), itemData=itemData, customer_in_session=customer_in_session  )
+            # Grpcart.query.filter_by(sectionid=str(prod_name[0])).update(product_img=str(prod_name[2]))
+            # db.session.commit()
+        else:
+            grpcart = Grpcart(sectionid=str(prod_name[0]),product_name=str(prod_name[1]),product_img=str(prod_name[2]),hostid=str(prod_name[3]))
+            db.session.add(grpcart)
+            db.session.commit()
 
 
-@app.route('/login', methods = ['GET','POST'])
+    prodname = [Product.query.all()[i].product_name for i in range(len(Product.query.all()))]
+
+    prod_id = [Product.query.all()[i].id for i in range(len(Product.query.all()))]
+
+    room = [Grpchat.query.all()[i].hostid for i in range(len(Grpchat.query.all()))]
+
+    members = [Grpchat.query.all()[i].memberid for i in range(len(Grpchat.query.all()))]
+    # print(room)
+
+    return render_template('home.html', prodname = prodname, prod_id=prod_id,room = room[0], members = members)
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        username = request.form['username']
+        password = request.form['password']
+        hashed_pswd = hashlib.md5(password.encode()).hexdigest()
+
+        user = Customer(fname=fname, lname=lname, username=username, password=hashed_pswd)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html")
+
+@app.route("/login",methods=['GET','POST'])
 def login():
     error = None
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        con = sqlite3.connect('database1.db')
-        cur = con.cursor()
-        cur.execute('SELECT username, password FROM users')
-        data = cur.fetchall()
-
-        # checks if the credentials match with the database
-        for row in data:
-            if row[0] == username and row[1] == hashlib.md5(password.encode()).hexdigest():
-                session["username"] = username
-                return redirect(url_for('home1', username=username) )
+        u = request.form['username']
+        ulist = [Customer.query.all()[i].username for i in range(len(Customer.query.all()))]
+        if u in ulist:
+            z = Customer.query.filter_by(username=u).all()[0].password
+            if(hashlib.md5(request.form['password'].encode()).hexdigest()!=z):
+                error = 'Invalid'
             else:
-                error = 'Invalid Credentials. Please try again.'
-            
-    return render_template("login.html", error=error)
+                session['username']=u
+                return redirect(url_for('home'))
+        error = 'Invalid'
+    return render_template('login.html', error=error)
 
-
-# page for registeration
-@app.route('/register', methods = ['GET','POST'])
-def register():
-    if request.method == 'POST':
-        if request.form['username'] != "" and request.form['password'] != "":
-            username = request.form["username"]
-            password = request.form["password"]
-            email = request.form['email']
-            phone = request.form['phone']
-            conn = sqlite3.connect("database1.db")
-            c = conn.cursor()
-            c.execute('INSERT INTO users (password, email, username, phone) VALUES (?, ?, ?, ?)', (hashlib.md5(password.encode()).hexdigest(), email, username, phone))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('login') )
-
-    return render_template("register.html")
-
-
-
-@app.route('/cart')
+@app.route("/cart")
 def cart():
-    # maintains list for users to be displayed in group cart
-    customer_in_session = []
+    sections = [Grpcart.query.all()[i].sectionid for i in range(len(Grpcart.query.all()))]
+    prod = [Grpcart.query.all()[i].product_name for i in range(len(Grpcart.query.all()))]
+    img = [Grpcart.query.all()[i].product_img for i in range(len(Grpcart.query.all()))]
+    print(img[0])
+    # print(testing)
+    return render_template('cart.html',length=3,sections=sections,prod=prod,img=img)
 
-    con = sqlite3.connect('database1.db')
-    cur = con.cursor()
-    cur.execute('SELECT name FROM kart')
-    data = cur.fetchall()
-    cur.execute('SELECT * FROM kart')
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('welcome'))
 
-    # maintaining a list of name,product name and image url
-    testing = cur.fetchall()
-    length = len(testing)
+@socketio.on('message')
+def handle_message(data):
+    # send({'msg': data['msg'], 'username': data['username'], 'room': data['room'] })
+    send(data, broadcast=True)
+    print(data)
+    grp = Grpchat(hostid=data["room"], memberid=data["username"])
+    db.session.add(grp)
+    db.session.commit()
 
-    # testing = [['Rohan Mehta', 'Grey Sweater', '/static/pics_1.jpg'],['Alex Simpson', 'Black Top', '/static/pics2.jpg'],['Marishka Menon', 'Kurti', '/static/pic3.jpg']]
+
+@socketio.on('new_room')
+def new_room(data):
+    room = data['room']
+    host = data['username']
+    print(data)
+
+    grp = Grpchat(hostid=host, memberid=host)
+    db.session.add(grp)
+    db.session.commit()
+
+@socketio.on('join')
+def join(data):
+    """User joins a room"""
+    username = data["username"] 
     
-    for i in data:
-        customer_in_session.append(str(i)[2:-3])
+    join_room(data["room"])
     
 
-    return render_template("cart.html", customer_in_session = customer_in_session, testing=testing, length=length )
+    grp = Grpchat(hostid=data["room"], memberid=username)
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
+
+
+
